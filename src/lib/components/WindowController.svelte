963104300154d9ef4665760a391a3d204ef76141
stefan <stefan.christou@gmail.com>
2023-01-11 20:32:19 +0100

    Refactor links into separate file for readability

--- src/lib/components/WindowController.svelte
+++ src/lib/components/WindowController.svelte
@@ -1,73 +1,15 @@
 <script>
-	import NavigationBar from './NavigationBar.svelte';
-	import Window from './Window.svelte';
-	import Words from './window-contents/Words.svelte';
-	import Work from './window-contents/Work.svelte';
-	import Who from './window-contents/Who.svelte';
-	import { onMount } from 'svelte';
-	import BoomiDataVis from './window-contents/work-pages/BoomiDataVis.svelte';
-	import ComicSans from './window-contents/work-pages/ComicSans.svelte';
-	import NGSBuild from './window-contents/work-pages/NGSBuild.svelte';
-	import ElSayHPark from './window-contents/work-pages/ElSayHPark .svelte';
-	import MerchantGourmet from './window-contents/work-pages/MerchantGourmet.svelte';
-	import BakerHughesDataVis from './window-contents/work-pages/BakerHughesDataVis.svelte';
-	import { mainPaths, workPaths } from '../../routes/site-paths.ts';
+  import {onMount} from 'svelte';
+  import NavigationBar from './NavigationBar.svelte';
+  import Window from './Window.svelte';
+  import {mainLinks, allLinks} from './WindowControllerLinks';
 
-	const workLinks = {
-		[workPaths.boomi]: {
-			text: 'Boomi',
-			component: BoomiDataVis,
-			initWidth: 1200,
-			initHeight: 900
-		},
-		[workPaths.comicUltraLight]: {
-			text: 'CS Ultralight',
-			component: ComicSans,
-			initWidth: 800,
-			initHeight: 900
-		},
-		[workPaths.ngs]: {
-			text: 'NGS',
-			component: NGSBuild,
-			initWidth: 800,
-			initHeight: 900
-		},
-		[workPaths.elPark]: {
-			text: 'El Say h’ Park',
-			component: ElSayHPark,
-			initWidth: 800,
-			initHeight: 900
-		},
-		[workPaths.merchantGourmet]: {
-			text: 'Merchant Gourmet',
-			component: MerchantGourmet,
-			initWidth: 800,
-			initHeight: 900
-		},
-		[workPaths.bakerHughes]: {
-			text: 'Baker Hughes',
-			component: BakerHughesDataVis,
-			initWidth: 800,
-			initHeight: 900
-		}
-	};
 
-	const mainLinks = {
-		[mainPaths.words]: { text: 'Words', component: Words, initWidth: 1200, initHeight: 900 },
-		[mainPaths.work]: {
-			text: 'Work',
-			component: Work,
-			initWidth: 1200,
-			initHeight: 900,
-			links: workLinks
-		},
-		[mainPaths.qAndA]: { text: 'Q&A', component: Who, initWidth: 800, initHeight: 800 }
-	};
 
-	const allLinks = {
-		...mainLinks,
-		...workLinks
-	};
+
+
+
+
 
 	let openWindows = new Map();
 	let open;
---
+++ src/lib/components/WindowControllerLinks.ts
@@ -0,0 +1,66 @@
+import {mainPaths, workPaths} from "../../routes/site-paths";
+import Words from './window-contents/Words.svelte';
+import Work from './window-contents/Work.svelte';
+import Who from './window-contents/QAndA.svelte';
+import BoomiDataVis from './window-contents/work-pages/BoomiDataVis.svelte';
+import ComicSans from './window-contents/work-pages/ComicSans.svelte';
+import NGSBuild from './window-contents/work-pages/NGSBuild.svelte';
+import ElSayHPark from './window-contents/work-pages/ElSayHPark.svelte';
+import MerchantGourmet from './window-contents/work-pages/MerchantGourmet.svelte';
+import BakerHughesDataVis from './window-contents/work-pages/BakerHughesDataVis.svelte';
+
+export const workLinks = {
+  [workPaths.boomi]: {
+    text: 'Boomi',
+    component: BoomiDataVis,
+    initWidth: 1200,
+    initHeight: 900
+  },
+  [workPaths.comicUltraLight]: {
+    text: 'CS Ultralight',
+    component: ComicSans,
+    initWidth: 800,
+    initHeight: 900
+  },
+  [workPaths.ngs]: {
+    text: 'NGS',
+    component: NGSBuild,
+    initWidth: 800,
+    initHeight: 900
+  },
+  [workPaths.elPark]: {
+    text: 'El Say h’ Park',
+    component: ElSayHPark,
+    initWidth: 800,
+    initHeight: 900
+  },
+  [workPaths.merchantGourmet]: {
+    text: 'Merchant Gourmet',
+    component: MerchantGourmet,
+    initWidth: 800,
+    initHeight: 900
+  },
+  [workPaths.bakerHughes]: {
+    text: 'Baker Hughes',
+    component: BakerHughesDataVis,
+    initWidth: 800,
+    initHeight: 900
+  }
+};
+
+export const mainLinks = {
+  [mainPaths.words]: { text: 'Words', component: Words, initWidth: 1200, initHeight: 900 },
+  [mainPaths.work]: {
+    text: 'Work',
+    component: Work,
+    initWidth: 1200,
+    initHeight: 900,
+    links: workLinks
+  },
+  [mainPaths.qAndA]: { text: 'Q&A', component: Who, initWidth: 800, initHeight: 800 }
+};
+
+export const allLinks = {
+  ...mainLinks,
+  ...workLinks
+};
