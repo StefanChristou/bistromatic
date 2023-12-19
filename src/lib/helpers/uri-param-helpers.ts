cff86c7ae0bb111602636edff2b55068fb19557a
stefanchristou <stefan.christou@gmail.com>
2023-12-19 18:05:58 +0100

    Update uri-param-helpers.ts

--- src/lib/helpers/uri-param-helpers.ts
+++ src/lib/helpers/uri-param-helpers.ts
@@ -1,10 +1,17 @@
-export function setSearchParam(param: string, value: string) {
-  const url = new URL(window.location.href);
-  url.searchParams.set(param, value);
-  window.history.pushState({}, '', url.toString());
+export function setSearchParam(key: string, value: string) {
+  if (!window) {
+    return;
+  }
+  const searchParams = new URLSearchParams(window.location.search);
+  searchParams.set(key, value);
+  const path = window.location.protocol + "//" + window.location.host + window.location.pathname + '?' + searchParams.toString();
+  window.history.pushState({path},'', path);
 }
 
 export function getSearchParam(param: string) {
+  if (!window) {
+    return;
+  }
   const url = new URL(window.location.href);
   return url.searchParams.get(param);
 }
